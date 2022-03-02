d7d3b08ad01899d3242dc5642e13961cde5913e9
Ken Yokohama <kenlopezyokohama@gmail.com>
2022-03-02 18:38:15 +0800

    Added logic to display modal when comments of tweet is clicked

--- src/components/Tweet.jsx
+++ src/components/Tweet.jsx
@@ -8,6 +8,11 @@ import "./tweet.css";
 import { deleteDoc, doc, updateDoc } from "firebase/firestore";
 import { auth, db } from "../firebase-config";
 import DeleteForeverTwoToneIcon from "@mui/icons-material/DeleteForeverTwoTone";
+import Backdrop from "@mui/material/Backdrop";
+import Box from "@mui/material/Box";
+import Modal from "@mui/material/Modal";
+import Fade from "@mui/material/Fade";
+import Typography from "@mui/material/Typography";
 
 function Tweet({
     tweetCollectionRef,
@@ -36,6 +41,23 @@ function Tweet({
         const targetTweetRef = doc(db, "tweets", id);
         await deleteDoc(targetTweetRef);
     };
+
+    // Comments Modal
+    const style = {
+        position: "absolute",
+        top: "50%",
+        left: "50%",
+        transform: "translate(-50%, -50%)",
+        width: 400,
+        bgcolor: "background.paper",
+        border: "2px solid #000",
+        boxShadow: 24,
+        p: 4,
+    };
+
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
 
     return (
         <div>
@@ -90,7 +112,7 @@ function Tweet({
                         }}
                     >
                         {/* Comments */}
-                        <div className="tweet-menu">
+                        <div className="tweet-menu" onClick={handleOpen}>
                             <div className="tweet-menu-icons">
                                 <CommentOutlinedIcon
                                     fontSize="small"
@@ -162,6 +184,36 @@ function Tweet({
                     </div>
                 </div>
             </div>
+            <Modal
+                aria-labelledby="transition-modal-title"
+                aria-describedby="transition-modal-description"
+                open={open}
+                onClose={handleClose}
+                closeAfterTransition
+                BackdropComponent={Backdrop}
+                BackdropProps={{
+                    timeout: 500,
+                }}
+            >
+                <Fade in={open}>
+                    <Box sx={style}>
+                        <Typography
+                            id="transition-modal-title"
+                            variant="h6"
+                            component="h2"
+                        >
+                            Text in a modal
+                        </Typography>
+                        <Typography
+                            id="transition-modal-description"
+                            sx={{ mt: 2 }}
+                        >
+                            Duis mollis, est non commodo luctus, nisi erat
+                            porttitor ligula.
+                        </Typography>
+                    </Box>
+                </Fade>
+            </Modal>
         </div>
     );
 }
