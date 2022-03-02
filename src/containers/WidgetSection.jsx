f22c8aaffd49f540e118e745de8c3e45b998c503
Ken Yokohama <kenlopezyokohama@gmail.com>
2022-03-02 18:10:23 +0800

    Added Widgets

--- src/containers/WidgetSection.jsx
+++ src/containers/WidgetSection.jsx
@@ -1,15 +1,48 @@
+import { Box } from "@mui/material";
 import React, { useState } from "react";
 import "./widgetSection.css";
 
 function WidgetSection(props) {
+    const handleClickPortfolio = () => {
+        window.open("https://github.com/Ken-Yokohama/Twitter-Clone");
+    };
+
     return (
-        <div className="widgetContainer">
-            WidgetSection
-            <h1>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Architecto, dolore et. Tempore delectus magni laudantium quam
-                ipsa ullam harum voluptas.
-            </h1>
+        <div
+            className="widgetContainer"
+            style={{
+                padding: "1rem",
+                display: "flex",
+                flexDirection: "column",
+                gap: "1rem",
+            }}
+        >
+            <h2>Thanks for visiting my twitter clone project!</h2>
+            <h3 style={{ fontWeight: "100" }}>
+                Please feel free to view my other projects:
+            </h3>
+            <Box
+                sx={{
+                    backgroundColor: "#F7F9F9",
+                    borderRadius: "1rem",
+                    cursor: "pointer",
+                    ":hover": {
+                        bgcolor: "#F2F4F4",
+                    },
+                    padding: "0.5rem",
+                    display: "flex",
+                    flexDirection: "column",
+                    gap: "1rem",
+                }}
+                onClick={handleClickPortfolio}
+            >
+                <h4>Ken Yokohama - Portfolio</h4>
+                <img
+                    src="https://www.mediabistro.com/wp-content/uploads/2016/02/tips-on-getting-your-copywriter-portfolio-started.jpg"
+                    alt="portfolio-img"
+                    style={{ width: "100%", borderRadius: "1rem" }}
+                />
+            </Box>
         </div>
     );
 }
