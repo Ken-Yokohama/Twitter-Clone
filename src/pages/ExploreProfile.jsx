7a7f8c8a538da8b11f793708f2108ce182d787d4
Ken Yokohama <kenlopezyokohama@gmail.com>
2022-03-03 21:22:39 +0800

    Added Button with CSS & Functionality to show more tweets in explore section

--- src/pages/ExploreProfile.jsx
+++ src/pages/ExploreProfile.jsx
@@ -1,7 +1,69 @@
-import React, { useState } from "react";
+import { Box } from "@mui/material";
+import {
+    collection,
+    limit,
+    onSnapshot,
+    orderBy,
+    query,
+} from "firebase/firestore";
+import React, { useEffect, useState } from "react";
+import { db } from "../firebase-config";
 
 function ExploreProfile({ profileSearch }) {
-    return <div>{profileSearch}</div>;
+    const [tweets, setTweets] = useState([]);
+
+    const [queryLimit, setQueryLimit] = useState(10);
+
+    const tweetCollectionRef = collection(db, "tweets");
+    const q = query(
+        tweetCollectionRef,
+        orderBy("date", "desc"),
+        limit(queryLimit)
+    );
+
+    useEffect(() => {
+        const unsub = onSnapshot(q, (snapshot) => {
+            setTweets(
+                snapshot.docs.map((tweets) => ({
+                    ...tweets.data(),
+                    id: tweets.id,
+                }))
+            );
+        });
+        return unsub;
+    }, [queryLimit]);
+
+    return (
+        <div>
+            {profileSearch}
+            {tweets.map((tweet) => (
+                <div key={tweet.id}>{tweet.author}</div>
+            ))}
+
+            {/* Show More Button */}
+            {tweets.length + 1 > queryLimit && (
+                <Box
+                    onClick={() => {
+                        setQueryLimit((prevValue) => prevValue + 10);
+                    }}
+                    sx={{
+                        display: "flex",
+                        alignItems: "center",
+                        justifyContent: "center",
+                        height: "75px",
+                        cursor: "pointer",
+                        color: "#43ABF2",
+                        transition: "background 0.25s",
+                        "&:hover": {
+                            backgroundColor: "rgb(29,155,240, .1)",
+                        },
+                    }}
+                >
+                    Show More
+                </Box>
+            )}
+        </div>
+    );
 }
 
 export default ExploreProfile;
