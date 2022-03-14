ea5813481ca8896823b4c9ef37b19a6110eb8c52
Ken Yokohama <kenlopezyokohama@gmail.com>
2022-03-14 19:00:35 +0800

    Added Responsiveness to Notifications Section

--- src/pages/Notifications.jsx
+++ src/pages/Notifications.jsx
@@ -2,6 +2,7 @@ import { collection, getDocs, orderBy, query } from "firebase/firestore";
 import React, { useEffect, useState } from "react";
 import { db } from "../firebase-config";
 import TwitterIcon from "@mui/icons-material/Twitter";
+import { Box } from "@mui/material";
 
 function Notifications(props) {
     const [registeredUsers, setRegisteredUsers] = useState([]);
@@ -31,36 +32,51 @@ function Notifications(props) {
             >
                 <h4>All Latest Registered Users</h4>
             </div>
-            {registeredUsers.map((user) => (
-                <div
-                    key={user.user}
-                    style={{
-                        height: "4rem",
-                        display: "flex",
-                        alignItems: "center",
-                        padding: "0.5rem 1rem",
-                        gap: "1rem",
-                    }}
-                >
-                    <TwitterIcon sx={{ fontSize: "2rem", color: "#1D9BF0" }} />
-                    <h4 style={{ fontWeight: "400" }}>
-                        There was a new user created with an account name{" "}
-                        <span
-                            style={{
-                                fontWeight: "400",
-                                color: "#6ABDF5",
-                            }}
-                        >
-                            @
-                            {user.user.substring(0, user.user.lastIndexOf("@"))}{" "}
-                        </span>
-                        on {user.timestamp?.toDate()?.toDateString()}{" "}
-                    </h4>
-                    {/* <h2>
-                        {user.timestamp?.toDate()?.toDateString()} - {user.user}
-                    </h2> */}
-                </div>
-            ))}
+            <Box
+                sx={{
+                    display: "flex",
+                    flexDirection: "column",
+                    "@media(max-width: 700px)": { gap: "0.5rem" },
+                    "@media(max-width: 500px)": { gap: "2rem" },
+                    "@media(max-width: 400px)": { gap: "3rem" },
+                }}
+            >
+                {registeredUsers.map((user) => (
+                    <div
+                        key={user.user}
+                        style={{
+                            height: "4rem",
+                            display: "flex",
+                            alignItems: "center",
+                            padding: "0.5rem 1rem",
+                            gap: "1rem",
+                        }}
+                    >
+                        <TwitterIcon
+                            sx={{ fontSize: "2rem", color: "#1D9BF0" }}
+                        />
+                        <h4 style={{ fontWeight: "400" }}>
+                            There was a new user created with an account name{" "}
+                            <span
+                                style={{
+                                    fontWeight: "400",
+                                    color: "#6ABDF5",
+                                }}
+                            >
+                                @
+                                {user.user.substring(
+                                    0,
+                                    user.user.lastIndexOf("@")
+                                )}{" "}
+                            </span>
+                            on {user.timestamp?.toDate()?.toDateString()}{" "}
+                        </h4>
+                        {/* <h2>
+                            {user.timestamp?.toDate()?.toDateString()} - {user.user}
+                        </h2> */}
+                    </div>
+                ))}
+            </Box>
         </div>
     );
 }
